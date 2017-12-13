100db124806e4da864ddd48febbcac1dd12d75d3
xotronal <xotronal@gmail.com>
2017-12-14 01:55:47 +0330

    Added Alvan.py

---
+++ Alvan.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import telebot
+import types from telebot
+
+Token=""
+bot = telebot.TeleBot(TOKEN)
+admin =
+channel =
+rediss = redis.StrictRedis(host='localhost', port=6379, db=0)
